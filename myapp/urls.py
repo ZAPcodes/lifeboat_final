1f1583843557476f97c00c32db686f17f9d8c2e3
ZAPcodes <techboyabhinav@gmail.com>
2024-09-21 15:52:44 +0530

    form_1

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import receive_data, display_data,get_latest_data
+from .views import receive_data, display_data,get_latest_data,form
 
 urlpatterns = [
     path('receive-data/', receive_data, name='receive_data'),
     path('display-data/', display_data, name='display_data'),
     path('latest-data/', get_latest_data, name='get_latest_data'),
-    path('form/',)
+    path('form/',form,name='form'),
 ]
